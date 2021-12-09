a0be4749f751fc2c5c5b5205af78d6479580024e
cre8iveMynd <95717319+cre8iveMynd@users.noreply.github.com>
2021-12-09 16:01:14 -0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-  picl static void main(String args[]){
+  public static void main(String args[]){
   //Prints Hello World 
   System.out.println("Hello World from Jenkins");
   }
